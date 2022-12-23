ae2cc38562174d64e3330593f5be3c763d335d39
marlo22 <marcin.1993@poczta.fm>
2022-12-23 13:22:28 +0100

    Debuggin

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const ELEMENTS_IDS = {
   gpsPositionY: 'gpsPositionY',
 }
 
+window.alert(navigator);
+
 navigator.geolocation.watchPosition(
   (position) => {
+    window.alert(JSON.stringify(position));
     document.getElementById(ELEMENTS_IDS.gpsPositionX).textContent = position.coords.latitude;
     document.getElementById(ELEMENTS_IDS.gpsPositionY).textContent = position.coords.longitude;
   },
