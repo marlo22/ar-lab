cdeea9753cc49c6d6a45ceb10f1103aaf0c546e7
marlo22 <marcin.1993@poczta.fm>
2023-01-23 15:15:27 +0100

    Add AR GeoJSON preview

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-const ELEMENTS_IDS = {
-  gpsPositionX: 'gpsPositionX',
-  gpsPositionY: 'gpsPositionY',
-  coordsInput: 'coordsInput',
-  saveCoordsButton: 'saveCoordsButton',
-}
+const textAreaElement = document.querySelector('textarea');
+const buttonElement = document.querySelector('button');
 
-try {
-  navigator.geolocation.watchPosition(
-    (position) => {
-      document.getElementById(ELEMENTS_IDS.gpsPositionX).textContent = position.coords.latitude;
-      document.getElementById(ELEMENTS_IDS.gpsPositionY).textContent = position.coords.longitude;
-    },
-    (error) => {
-      document.getElementById(ELEMENTS_IDS.gpsPositionX).textContent = error.message;
-    }
-  );
-} catch (err) {
-  document.getElementById(ELEMENTS_IDS.gpsPositionX).textContent = err.message;
-}
+textAreaElement.addEventListener('change', (event) => {
+  buttonElement.disabled = !event.target.value;
+});
 
-const setArObjectAttributes = (element, attributes) => {
-  Object.entries(attributes).forEach(([name, value]) => {
-    element.setAttribute(name, value);
-  });
-}
-
-const addArObject = (coordinate) => {
-  document.querySelector('a-scene').insertAdjacentHTML('beforeEnd', `<a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: ${coordinate[0]}; longitude: ${coordinate[1]}" scale="10 10 10"></a-entity>`);
-};
-
-document.getElementById(ELEMENTS_IDS.saveCoordsButton).addEventListener('click', () => {
-  // document.querySelectorAll('a-entity').forEach((element) => element.remove()); // TOOD: Add remove of the previous entities.
-
-  const coordinate = document.getElementById(ELEMENTS_IDS.coordsInput).value;
-  if (coordinate) addArObject(coordinate.split(','));
+buttonElement.addEventListener('click', () => {
+  location = `/viewer/index.html?d=${JSON.stringify(JSON.parse(textAreaElement.value))}`
 });
---
+++ viewer/viewer.js
@@ -0,0 +1,59 @@
+const getGeoJsonFromQueryParams = () => {
+  const queryParams = new URLSearchParams(location.search);
+  const stringifiedGeoJson = queryParams.get("d");
+  if (!stringifiedGeoJson) {
+    alert("Brak danych do wyświetlenia!");
+    location = "/";
+  }
+  return JSON.parse(stringifiedGeoJson);
+};
+
+window.onload = () => {
+  const cameraElement = document.querySelector("[gps-new-camera]");
+
+  cameraElement.addEventListener("gps-camera-update-position", () => {
+    getGeoJsonFromQueryParams().features.forEach(({ geometry, properties }) => {
+      const compoundEntity = document.createElement("a-entity");
+
+      const [longitude, latitude] = geometry.coordinates;
+      compoundEntity.setAttribute("gps-new-entity-place", {
+        latitude,
+        longitude,
+      });
+
+      const markerIconImage = document.createElement("a-image");
+      markerIconImage.setAttribute("look-at", "[gps-new-camera]");
+      markerIconImage.setAttribute("src", "../assets/locator.png");
+      markerIconImage.setAttribute("width", 6);
+      markerIconImage.setAttribute("height", 10);
+      markerIconImage.setAttribute("position", {
+        x: 0,
+        y: 7,
+        z: 0,
+      });
+
+      const labelPlane = document.createElement("a-plane");
+      labelPlane.setAttribute("color", "#115f8b");
+      labelPlane.setAttribute("width", 3);
+      labelPlane.setAttribute("height", "auto");
+      labelPlane.setAttribute("look-at", "[gps-new-camera]");
+      labelPlane.setAttribute("scale", {
+        x: 20,
+        y: 20,
+        z: 20,
+      });
+      labelPlane.setAttribute("text", {
+        // TODO: Obsłużyć polskie znaki.
+        // font: 'https://cdn.aframe.io/fonts/mozillavr.fnt',
+        value: properties.name || `${longitude}, ${latitude}`,
+        color: "#fff",
+        align: "center",
+        width: 3.3,
+      });
+
+      compoundEntity.appendChild(labelPlane);
+      compoundEntity.appendChild(markerIconImage);
+      document.querySelector("a-scene").appendChild(compoundEntity);
+    });
+  });
+};
